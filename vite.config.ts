d90facdb76ce00431e5e7959a0885632b70af757
Michal Vodicka <vodickam@gmail.com>
2023-07-31 19:45:10 +0200

    chore(tooling): module resolution

--- vite.config.ts
+++ vite.config.ts
@@ -10,4 +10,9 @@ export default defineConfig({
       ssr: { enable: true },
     }),
       svelte()],
+  resolve: {
+    alias: {
+      'typography': '/src/components/design/typography',
+    }
+  }
 })
