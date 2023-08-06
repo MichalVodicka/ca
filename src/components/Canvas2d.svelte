f4c57cb3d39d535c53946298d53a6f35e6af8854
Michal Vodicka <vodickam@gmail.com>
2023-08-06 16:17:48 -0400

    feat(2d): wip poc

--- src/components/Canvas2d.svelte
+++ src/components/Canvas2d.svelte
@@ -4,6 +4,12 @@
     let x: number = 40
     let y: number = 40
 
+    let genId = 0;
+    let initState: number[][];
+    let state: Map<number, number[]> = new Map(Array(x).map((el, idx) => [idx,[]]));
+
+    state.set(5,[1,9,18,25,35])
+
     let data: number[][] = [];
 
     for(let i = 0 ; i < x ; i++){
@@ -12,13 +18,12 @@
             data[i][j] = 0;
         }
     }
-
 </script>
 
-{#each data as row}
+{#each data as row, iRow}
     <div class="flex">
-        {#each row as el}
-            <Cell state={!!el}/>
+        {#each row as cell, iCell}
+            <Cell state={state.get(iRow).includes(iCell)}/>
         {/each}
     </div>
 {/each}
