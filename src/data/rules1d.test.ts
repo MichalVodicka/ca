7531526c6b968804dd589af4600e3c3898ec1151
Michal Vodicka <vodickam@gmail.com>
2023-08-07 10:43:58 -0400

    chore(1d): improve comments

---
+++ src/data/rules1d.test.ts
@@ -0,0 +1,6 @@
+import { expect, test } from 'vitest'
+import  rulefac  from './rules1d'
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(rulefac(34)).toBe(3)
+})
--- src/data/rules1d.ts
+++ src/data/rules1d.ts
@@ -1,10 +1,27 @@
-const rules = {}
+// rules: number -> array of 8 rule states
+//  1: {
+//   "0": 1,
+//   "1": 0,
+//   "2": 0,
+//   "3": 0,
+//   "4": 0,
+//   "5": 0,
+//   "6": 0,
+//   "7": 0
+// }
 
-for (let i = 1; i<256 ;i++){
-    const rule = (i>>>0).toString(2).padStart(8,'0')
-    rules[i] = Object.assign({}, [...rule].map(Number).reverse())
+const rules: Record<number, Record<number, number>> = {}
+
+type IRule = [string, string, string, string, string, string, string, string]
+
+// i = rule number
+for (let i = 1; i < 256; i++) {
+    // a rule is converted to 8 binary states
+    const rule = [...(i.toString(2).padStart(8, '0'))] as IRule
+    rules[i] = Object.assign({}, rule.map(Number).reverse())
 }
 
-const ruleFac = (ruleset) => (a, b, c) => rules[ruleset][(a << 2) + (b << 1) + (c << 0)]
+const ruleFac = (ruleset: number) => (a: boolean, b: boolean, c: boolean): number =>
+    rules[ruleset][(Number(a) << 2) + (Number(b) << 1) + (Number(c) << 0)]
 
 export default ruleFac
