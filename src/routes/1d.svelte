4de5d9fe52dee93cdbf0797cb50cc53f1c8b462a
Michal Vodicka <vodickam@gmail.com>
2023-08-17 21:44:14 +0200

    chore(1d): minor text fix

--- src/routes/1d.svelte
+++ src/routes/1d.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <H1>1D ca</H1>
-<p>Elementary cellular automata</p>:
+<p>Elementary cellular automata</p>
 <div class="flex flex-col">
     <div>
         Rule: <input type="number" class="bg-amber-200 rounded p-1" bind:value={ruleNumber} />
