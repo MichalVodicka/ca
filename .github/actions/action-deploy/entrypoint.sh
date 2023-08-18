98b4b9946e3949a929ae35a84b8530e6bc70b794
Michal Vodicka <vodickam@gmail.com>
2023-08-18 16:53:22 +0200

    chore(devops): fix region is missing

--- .github/actions/action-deploy/entrypoint.sh
+++ .github/actions/action-deploy/entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/sh -l
+# fix workarround
+export AWS_EC2_METADATA_DISABLED=true
 
 aws s3 cp --recursive $DEPLOY_FROM $DEPLOY_TO$GITHUB_SHA/
 aws s3 cp --recursive $DEPLOY_FROM ${DEPLOY_TO}current/
